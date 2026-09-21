129b11735ff904ed547a2a39074c7d80280c6647
rocha06101 <ferpa.felipe2@gmail.com>
2026-09-21 13:11:53 -0300

    fix(auth): implement secure csrf and in-memory session flow

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideHttpClient, withInterceptors} from '@angular/common/http';
+import { ApplicationConfig, inject, provideAppInitializer } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth-interceptor';
 import { provideRouter } from '@angular/router';
 import { timeoutInterceptor } from './core/interceptors/timeout-interceptor';
 import { routes } from './app.routes';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { AuthService } from './core/services/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [
- provideHttpClient(
-    withInterceptors([authInterceptor, timeoutInterceptor])
-  ),
+    provideHttpClient(
+      withInterceptors([authInterceptor, timeoutInterceptor])
+    ),
+    provideAppInitializer(() => inject(AuthService).restoreSession()),
     provideRouter(routes),
     provideCharts(withDefaultRegisterables())
   ]
